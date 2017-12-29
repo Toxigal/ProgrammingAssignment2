daa438a3d07386f87f24bf6735071a0437e0c7c1
Toxigal <toxigal@gmail.com>
2017-12-28 22:08:30 -0500

    Toxigal Week 3 Assignment
    This is my completed assignment for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This pair of functions will cache the inverse of a matrix
 
-## Write a short comment describing this function
+## The first step is to create the matrix cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  ## The input into this function will be a matrix
+  inv <- NULL                                 ## Create an empty object to be used by the functio
+  set <- function(y){                         
+    x <<- y                                   ## Assign value of y to x in the parent environment
+    inv <<- NULL                              ## Clear value of inv from previous use of CacheSolve
+  }
+  get <- function() x                         ## Retrieve x from parent environment
+  set_inv <- function(solveMatrix) inv_matrix <<- inv  ##Createfunction to solve inverse and assign value of inv to inv_matrix in parent environment
+  get_inv <- function() inv                            ##Retrieve inv from parent environment
+  list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)  ##assign functions to list in parent environment
 }
 
+## This function retrieves the inverse matrix from makeCacheMarix()
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) {    ## input is x from parent environment    
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inv()                ## Runs get_inv function on x
+  if(!is.null(inv)){                ## if result is not null (set to null by MakeCacheVector when there is no valid cache)
+    message("getting cached data")  ## Uses cached data
+    return(inv)
+  }
+  inverse_matrix<- x$get()          ##assign matrix from input to inverse_matrix
+  inv <- solve(inverse_matrix)      ## calculate inverse of matrix and assign to inv
+  x$set_inv(inv)                    ##return result to parent environment
+  inv      
 }
